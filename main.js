fd87143c406fd6cf229f70efe0000fcf0dcc550d
platypause <jose.a.plata@gmail.com>
2016-01-27 21:07:59 -0500

    Assignment 1

---
+++ main.js
@@ -0,0 +1,14 @@
+var myHobbies = ["Playing Guitar", 
+				"Taking Photographs", 
+				"Designing and Developing Web Applications", 
+				"Online Learning", 
+				"Reading", 
+				"Watching films", 
+				"Going to the gym"
+				];
+
+for (var i = 0; i < myHobbies.length; i++) {
+	console.log(myHobbies[i]);
+};
+
+console.log("...These Are My Hobbies");
